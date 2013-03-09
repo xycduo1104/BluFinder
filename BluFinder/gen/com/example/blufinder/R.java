bce72d6daa45038be77a7418ecd3071d612f3026
Yachen Xu <xycduo1104@gmail.com>
2013-03-08 22:08:20 -0800

    v0.1.1
    -Added: 
    	Splash Activity
    
    -Changed: 
    	Adjusted minsdkversion to 8 (Froyo)
    	Modified UI layout for Large/Xlarge
    
    Signed-off-by: Yachen Xu <xycduo1104@gmail.com>

--- BluFinder/gen/com/example/blufinder/R.java
+++ BluFinder/gen/com/example/blufinder/R.java
@@ -11,6 +11,8 @@ public final class R {
     public static final class anim {
         public static final int animation_enter=0x7f040000;
         public static final int animation_leave=0x7f040001;
+        public static final int mainfadein=0x7f040002;
+        public static final int splashfadeout=0x7f040003;
     }
     public static final class attr {
     }
@@ -33,9 +35,10 @@ public final class R {
         public static final int Text5=0x7f080007;
         public static final int Text6=0x7f080008;
         public static final int helpButton=0x7f08000f;
-        public static final int menu_settings=0x7f080010;
+        public static final int menu_settings=0x7f080011;
         public static final int notifyText=0x7f08000b;
         public static final int okButton=0x7f080001;
+        public static final int progressBar1=0x7f080010;
         public static final int ringtoneButton=0x7f08000e;
         public static final int ringtoneCheck=0x7f08000c;
         public static final int serviceButton=0x7f080009;
@@ -46,18 +49,18 @@ public final class R {
     public static final class layout {
         public static final int activity_help=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int activity_splash=0x7f030002;
     }
     public static final class menu {
         public static final int activity_help=0x7f070000;
         public static final int activity_load_screen=0x7f070001;
         public static final int activity_main=0x7f070002;
+        public static final int activity_splash=0x7f070003;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050003;
         public static final int menu_settings=0x7f050001;
         public static final int title_activity_help=0x7f050002;
-        public static final int title_activity_load_screen=0x7f050004;
     }
     public static final class style {
         /** 
